2440dd44bf9a2815d71a54231f74cb62468f1287
Uday207 <chinthamanuudaykumar@gmail.com>
2024-12-26 11:36:07 +0530

    commit

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useRef } from 'react';
 import { Footer, Navbar, Product } from "../components";
 
 const Products = () => {
-  const componentMounted = useRef(true); // Initialize a ref to track the mounted state
+  const componentMounted = useRef(null); // Initialize a ref to track the mounted state
 
   useEffect(() => {
-    console.log('Component mounted successfully!'); // Your logic when component mounts
+    componentMounted.current = true; // Set to true when the component is mounted
+    console.log('Component mounted successfully!'); // Your logic when the component mounts
 
     return () => {
-      componentMounted.current = false; // Cleanup when component unmounts
+      componentMounted.current = false; // Set to false when the component unmounts
       console.log('Component unmounted');
     };
   }, []); // Empty dependency array ensures this runs only on mount and unmount
